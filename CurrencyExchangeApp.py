a1a172967707e9b04f3d4af609d77c6e456717b9
Myszanik <dominikszymanik@gmail.com>
2024-09-07 12:12:12 +0100

    Update for GUI
    Added place to input amount. created two drop down menus for selection of currency

--- CurrencyExchangeApp.py
+++ CurrencyExchangeApp.py
@@ -33,9 +33,32 @@ class Currency_Exchange_App:
 
     def main_screen_frame(self):
         # Main screen label
-        self.description= tk.Label(self.main_frame, text='')
+        self.description= tk.Label(self.main_frame, text='Welcome!', font=('Papyrus', 60), fg='black', bg='beige')
+        self.description.place(x=415, y=5)
+        self.description = tk.Label(self.main_frame, text='Please choose currency you want to exchange and the amount', font=('Papyrus', 35), fg='black', bg='beige')
+        self.description.place(x=50, y=80)
+        self.from_currency_description = tk.Label(self.main_frame, text='Choose from currency', font=('Verdana', 20), bg='beige', fg='black')
+        self.from_currency_description.place(x=150, y=150)
+        # Create a variable to store the selected value
+        self.from_currency_var = tk.StringVar(self.main_frame)
+        self.from_currency_var.set("From")  # Default text
+        self.from_currency = tk.OptionMenu(self.main_frame, self.from_currency_var, *[0])
+        self.from_currency.config(bd=0, width=4, height=0, font=('Verdana', 25), highlightbackground="beige", highlightcolor="beige", highlightthickness=0, relief='flat')
+        self.from_currency.place(x=220, y=195)
+        self.entry_description = tk.Label(self.main_frame, text='Enter amount below', font=('Verdana', 20), bg='beige', fg='black')
+        self.entry_description.place(x=435, y=150)
+        self.entry = tk.Entry(self.main_frame, width=16, font=('Verdana', 25), bg='white', fg='black', insertbackground='black')
+        self.entry.place(x=405, y=190)  # Adjust x and y based on your window layout
+        self.entry.focus_set()  # Set focus to the entry widget
+        self.to_currency_description = tk.Label(self.main_frame, text='Choose to currency', font=('Verdana', 20), bg='beige', fg='black')
+        self.to_currency_description.place(x=700, y=150)
+        self.to_currency_var = tk.StringVar(self.main_frame)
+        self.to_currency_var.set("To")  # Default text
+        self.to_currency = tk.OptionMenu(self.main_frame, self.to_currency_var, *[0])
+        self.to_currency.config(bd=0, width=4, height=0, font=('Verdana', 25), highlightbackground="beige", highlightcolor="beige", highlightthickness=0, relief='flat')
+        self.to_currency.place(x=760, y=195)
         self.created_by = tk.Label(self.main_frame, text='Created by Myszanik', font=('Arial', 20), bg='beige', fg='black')
-        self.created_by.place(x=440, y=700)
+        self.created_by.place(x=440, y=710)
 
     def switch_scene(self):
         # Hide the welcome frame and show the main frame
